4a3213f77079c897ce4e6f38a469430847b8c544
Raman Tayal <42838224+ramantayal12@users.noreply.github.com>
2024-01-27 02:17:19 +0530

    MongoDb Starter Integration Working  (#2)
    * swagger integration working
    
    * mongodb integration working

---
+++ src/main/java/org/example/repository/BookRepository.java
@@ -0,0 +1,10 @@
+package org.example.repository;
+
+import org.example.models.Book;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface BookRepository extends MongoRepository<Book, String> {
+
+  public Book findByFirstName(String firstName);
+  public Book findByLastName(String lastName);
+}
